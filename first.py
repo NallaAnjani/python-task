34b3c6280e2b147e5ef53b8ec20e12464f276b89
NallaAnjani <112603890+NallaAnjani@users.noreply.github.com>
2025-02-05 11:34:37 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,73 @@
+# interger: it is a one of the datatype 
+# which are whole numbers it can be considered as interger
+# ex:
+a=10
+b=20
+print(a)
+print(b)
+
+# addition of two numbers
+print(a+b)
+# subtraction of two numbers
+print(a-b)
+# multiplication of two numbers
+print(a*b)
+# division of two numbers
+print(a/b)
+# exponential of two numbers(a power b)
+print(a**b)
+# floor division of two numbers it returns the integer  part from the output
+print(a//b)
+# subtraction of two numbers
+# modulus of two numbers (its returns reminder )
+print(a%b)
+
+
+
+# float: the numbers which have decimal values
+# ex:
+num1=0.5
+num1=7.8
+print(num1)
+print(num2)
+
+
+
+# type: it defines the type of a datatype 
+# ex:
+type(a)
+type(num1)
+
+# string: it is collection of character enclosed in a single quotes or double  quotes
+# spaces also taken by the string as a charcter
+# ex:
+str1="hello"
+str2="  "
+print(str1)
+print(str2)
+
+# length:it counts how many characters are present in the string
+# ex:
+aa="raju"
+bb="mahesh babu"
+print(len(aa))
+print(len(bb))
+
+# index:accessing the  one character from the string by using the indexing
+# index value  starts form 0
+# ex:
+print(aa[1])
+print(bb[7])
+
+# slicing:if you want to accent the more than one character then use slicing
+# ex:
+str3="hello hyderbad" 
+print(name[0:5])
+print(name[ :9])  
+print(name[1:])  
+print(name[-2:-8:-1])     
+print(name[-1:-6: -2])  
+print(name[0:10:2])  
+
+
+
